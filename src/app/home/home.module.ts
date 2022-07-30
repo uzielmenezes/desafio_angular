d21fb458a2700bdbec94ed8409705b6b8fae14f4
uzielmenezes <uzielcivil@gmail.com>
2022-07-30 19:20:55 -0300

    message validations

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,10 +5,11 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
+import { MessageModule } from '../components/message/message.module';
 
 @NgModule({
   declarations: [HomeComponent, LoginComponent],
-  imports: [CommonModule, HomeRoutingModule, FormsModule],
+  imports: [CommonModule, HomeRoutingModule, FormsModule, MessageModule],
   exports: [HomeComponent],
 })
 export class HomeModule {}
