2151f08707133af8e3a03007cba291456b2de19d
uzielmenezes <uzielcivil@gmail.coom>
2022-08-04 21:58:13 -0300

    header template

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/authentication/user/user.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent {
+  user$ = this.userService.returnUser();
+
+  constructor(private userService: UserService, private router: Router) {}
+
+  logout() {
+    this.userService.logout();
+    this.router.navigate(['']);
+  }
+}
