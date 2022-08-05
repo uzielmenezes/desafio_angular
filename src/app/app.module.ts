53c23caa7843a931752e96522d4db1ea96025b62
uzielmenezes <uzielcivil@gmail.coom>
2022-08-04 23:25:55 -0300

    header fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderModule } from './components/header/header.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, HeaderModule],
   providers: [],
   bootstrap: [AppComponent],
 })
