3b94c47e696f3c8f249f3a45fa7027410b18246b
uzielmenezes <uzielcivil@gmail.coom>
2022-08-12 15:42:05 -0300

    listing vehicles on dashboard, small module fix

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Vehicles } from '../models/vehicle.model';
+import { VehicleService } from './vehicle/vehicle.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  vehicleList: Vehicles;
 
-  constructor() { }
+  constructor(private vehicleServvice: VehicleService) {}
 
   ngOnInit(): void {
+    this.vehicleServvice.getVehicle().subscribe({
+      next: (data: any) => {
+        this.vehicleList = data['vehicles'];
+      },
+      error: (err) => {
+        alert('Ocorreu um erro!!');
+        console.log(err);
+      },
+    });
   }
-
 }
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
-import { HeaderModule } from '../components/header/header.module';
-import { HeaderComponent } from '../components/header/header.component';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule, HeaderModule],
-  exports: [HeaderComponent],
+  imports: [CommonModule],
 })
 export class DashboardModule {}
