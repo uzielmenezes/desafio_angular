92c8ebde2f183ce372c1dcf29b7e2fb729440854
uzielmenezes <uzielcivil@gmail.coom>
2022-08-15 16:59:00 -0300

    vehicleData model

---
+++ src/app/models/vehicleData.model.ts
@@ -0,0 +1,11 @@
+export interface VehicleData {
+  id: number;
+  vin: any;
+  odometer: any;
+  tirePressure: any;
+  status: any;
+  batteryStatus: any;
+  fuelLevel: any;
+  lat: any;
+  long: any;
+}
