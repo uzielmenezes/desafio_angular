753b50b2cc0bc5cf506ac8cb957a4245c9d7463b
uzielmenezes <uzielcivil@gmail.coom>
2022-08-23 16:38:53 -0300

    small fixes for models

--- src/app/models/vehicle.model.ts
+++ src/app/models/vehicle.model.ts
@@ -1,11 +1,11 @@
 export interface Vehicles extends Array<Vehicle> {}
 
 export interface Vehicle {
-  id: number | string;
-  vehicle: string;
-  volumetotal: number | string;
-  connected: number | string;
-  softwareUpdates: number | string;
+  id?: number | string;
+  vehicle?: string;
+  volumetotal?: number | string;
+  connected?: number | string;
+  softwareUpdates?: number | string;
 }
 
 export interface VehiclesAPI {
--- src/app/models/vehicleData.model.ts
+++ src/app/models/vehicleData.model.ts
@@ -1,15 +1,15 @@
 export interface VehiclesData extends Array<VehicleData> {}
 
 export interface VehicleData {
-  id: number | string;
-  vin: number | string;
-  odometer: number | string;
-  tirePressure: number | string;
-  status: string;
-  batteryStatus: string;
-  fuelLevel: number | string;
-  lat: number | string;
-  long: number | string;
+  id?: number | string;
+  vin?: number | string;
+  odometer?: number | string;
+  tirePressure?: number | string;
+  status?: string;
+  batteryStatus?: string;
+  fuelLevel?: number | string;
+  lat?: number | string;
+  long?: number | string;
 }
 
 export interface VehiclesDataAPI {
