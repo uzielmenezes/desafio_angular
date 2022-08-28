4323f50557792a23f11b3c49c99d4b53736347fe
uzielmenezes <uzielcivil@gmail.coom>
2022-08-28 10:41:04 -0300

    style changes

--- src/app/dashboard/vehicle-cards/vehicle-cards.component.ts
+++ src/app/dashboard/vehicle-cards/vehicle-cards.component.ts
@@ -12,20 +12,18 @@ export class VehicleCardsComponent {
   vehicles: Vehicle[] = [];
   selectedVehicleId: number = 0;
   selectedVehicle: Vehicle = {};
-  selectedVehicleDetails: VehicleData = {};
+  selectedVehicleData: VehicleData = {};
 
-  // quando a pagina carrega, inicia a busca pelos carros, ao finalizar atribui na variavel vehicles, usada para renderizar o select
   constructor(private vehicleService: VehicleService) {
     this.vehicleService
       .getVehicles()
       .subscribe((vehicles) => (this.vehicles = vehicles));
   }
 
-  // quando muda o select busca os detalhes do carro selecionado e atribui na variavel selectedVehicleDetails
   onChange(vehicleId: number) {
     this.selectedVehicle = this.vehicles[vehicleId - 1];
     this.vehicleService
       .getVehicleData(vehicleId)
-      .subscribe((vehicle) => (this.selectedVehicleDetails = vehicle));
+      .subscribe((vehicle) => (this.selectedVehicleData = vehicle));
   }
 }
